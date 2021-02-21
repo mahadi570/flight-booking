604144227689a2451310e4f8cd230734f60080e4
mahadi570 <mahadihasan18407@gmail.com>
2021-02-21 14:08:25 +0600

    Trying To DRY Code

--- setting.js
+++ setting.js
@@ -1,61 +1,58 @@
-        function handleTicket(ticket, isIncrease) {
-            const ticketInput = document.getElementById(ticket + '-ticket-count');
-            const ticketCount = parseInt(ticketInput.value);
-            let newTicket = ticketCount;
-            if (isIncrease == true) {
-                newTicket = ticketCount + 1;
-            }
-            if (isIncrease == false && ticketCount > 0) {
-                newTicket = ticketCount - 1;
-            }
-            ticketInput.value = newTicket;
-            const fTicketInput = document.getElementById('f-ticket-count').value;
-            const eTicketInput = document.getElementById('e-ticket-count').value;
-            const total = fTicketInput * 150 + eTicketInput * 100;
-            document.getElementById('ticket-cost').innerText = total;
+function handleTicket(ticket, isIncrease) {
+    const ticketInput = document.getElementById(ticket + '-ticket-count');
+    const ticketCount = parseInt(ticketInput.value);
+    let newTicket = ticketCount;
+    if (isIncrease == true) {
+        newTicket = ticketCount + 1;
+    }
+    if (isIncrease == false && ticketCount > 0) {
+        newTicket = ticketCount - 1;
+    }
+    ticketInput.value = newTicket;
+    const fTicketInput = document.getElementById('f-ticket-count').value;
+    const eTicketInput = document.getElementById('e-ticket-count').value;
+    const total = fTicketInput * 150 + eTicketInput * 100;
+    document.getElementById('ticket-cost').innerText = total;
 
-            const tax = Math.round(total * 0.1);
-            document.getElementById('tax').innerText = tax;
+    const tax = Math.round(total * 0.1);
+    document.getElementById('tax').innerText = tax;
 
-            const totalCost = total + tax;
-            document.getElementById('total').innerText = totalCost;
-        };
+    const totalCost = total + tax;
+    document.getElementById('total').innerText = totalCost;
+};
 
-        document.getElementById('submit-btn').addEventListener('click', function () {
-            document.getElementById('booking').style.display = 'none';
-            document.getElementById('booked').style.display = 'block';
+function gettingValue(firstValue, secondValue) {
+    const getValue = document.getElementById(firstValue).value
+    document.getElementById(secondValue).innerText = getValue;
+}
 
-            const flyingFrom = document.getElementById('flying-from-text').value
-            document.getElementById('flying-from').innerText = flyingFrom;
+document.getElementById('submit-btn').addEventListener('click', function () {
+    document.getElementById('booking').style.display = 'none';
+    document.getElementById('booked').style.display = 'block';
 
-            const flyingTo = document.getElementById('flying-to-text').value
-            document.getElementById('flying-to').innerText = flyingTo;
+    gettingValue('flying-to-text', 'flying-to');
+    gettingValue('flying-from-text', 'flying-from');
+    gettingValue('departure-date', 'departure');
+    gettingValue('return-date', 'return');
 
-            const departure = document.getElementById('departure-date').value
-            document.getElementById('departure').innerText = departure;
+    const fTicket = document.getElementById('f-ticket-count').value
+    document.getElementById('f-ticket-qty').innerText = fTicket;
 
-            const returnDate = document.getElementById('return-date').value
-            document.getElementById('return').innerText = returnDate;
+    const fTicketCost = fTicket * 150;
+    document.getElementById('f-ticket-cost').innerText = fTicketCost;
 
-            const fTicket = document.getElementById('f-ticket-count').value
-            document.getElementById('f-ticket-qty').innerText = fTicket;
+    const eTicket = document.getElementById('e-ticket-count').value
+    document.getElementById('e-ticket-qty').innerText = eTicket;
 
-            const fTicketCost = fTicket * 150;
-            document.getElementById('f-ticket-cost').innerText = fTicketCost;
+    const eTicketCost = eTicket * 100;
+    document.getElementById('e-ticket-cost').innerText = eTicketCost;
 
-            const eTicket = document.getElementById('e-ticket-count').value
-            document.getElementById('e-ticket-qty').innerText = eTicket;
+    const total = fTicket * 150 + eTicket * 100;
+    document.getElementById('total-price').innerText = total;
 
-            const eTicketCost = eTicket * 100;
-            document.getElementById('e-ticket-cost').innerText = eTicketCost;
+    const tax = Math.round(total * 0.1);
+    document.getElementById('vat').innerText = tax;
 
-
-            const total = fTicket * 150 + eTicket * 100;
-            document.getElementById('total-price').innerText = total;
-
-            const tax = Math.round(total * 0.1);
-            document.getElementById('vat').innerText = tax;
-
-            const totalCost = total + tax;
-            document.getElementById('grand-total').innerText = totalCost;
-        });
+    const totalCost = total + tax;
+    document.getElementById('grand-total').innerText = totalCost;
+});
